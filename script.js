b9e7e8983d8b305dd9aeb9b687ce2c9ffece3a5d
Mr-Dostonbek <goyipovdoston0@gmail.com>
2021-07-29 10:57:12 +0500

    add 2-task finish.....

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
 "use strict";
 
-const numberOfNews = +prompt("Siz qancha yangilik ko`rdiz?");
+// const numberOfNews = +prompt("Siz qancha yangilik ko`rdiz?");
 
 const personalNewsDB = {
-  count: numberOfNews,
+  // count: numberOfNews,
   news: {},
   actors: {},
   genres: [],
+  shop: {},
   privat: false,
 };
 
-const a = prompt("Oxirgi ko`rgan yangiliklarizdan bir?"),
-      b = prompt("Unga qancha baho bergan bo`lar ediz?");
+// const a = prompt("Oxirgi ko`rgan yangiliklarizdan bir?"),
+//       b = prompt("Unga qancha baho bergan bo`lar ediz?");
 
-personalNewsDB.news[a] = b;
+const product = prompt("Nima xarid qilmoqchisiz?"),
+      number = +prompt("Nechta xarid qilmoqchisiz?");
+
+personalNewsDB.shop[product] = number;
+
+alert(`Sotib olganlariz narxi: ${number * 5000} so'm`);
+
+// personalNewsDB.news[a] = b;
 
 console.log(personalNewsDB);
