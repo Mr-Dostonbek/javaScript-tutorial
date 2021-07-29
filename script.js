5f02062facfe526c6a225c665f16bdadca250870
Mr-Dostonbek <goyipovdoston0@gmail.com>
2021-07-29 10:56:11 +0500

    add 1-task finish...

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 "use strict";
+
+const numberOfNews = +prompt("Siz qancha yangilik ko`rdiz?");
+
+const personalNewsDB = {
+  count: numberOfNews,
+  news: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+const a = prompt("Oxirgi ko`rgan yangiliklarizdan bir?"),
+      b = prompt("Unga qancha baho bergan bo`lar ediz?");
+
+personalNewsDB.news[a] = b;
+
+console.log(personalNewsDB);
