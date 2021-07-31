85369ce3796397bb0b2b4a57893314e9639f254c
Mr-Dostonbek <goyipovdoston0@gmail.com>
2021-07-31 22:53:14 +0500

    17-lesson string ....

---
+++ string.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const text = "Hello World";
+
+// console.log(text.toUpperCase());
+// console.log(text.toLowerCase());
+
+// console.log(text.indexOf("d"));
+
+// console.log(text.slice(6, 11));
+
+const number = 12.5;
+
+console.log(Math.round(number)); // sonni yaxlitlab beradi
+
+const borderWidth = "12px"; // stringni numberga o'tkazib beradi
+
+console.log(parseInt(borderWidth));
