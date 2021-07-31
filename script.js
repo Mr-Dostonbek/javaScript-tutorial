c668ccde31a584377664c9ff1b745a16228f62fc
Mr-Dostonbek <goyipovdoston0@gmail.com>
2021-07-31 10:18:37 +0500

    14-lesson While method finish....

--- script.js
+++ script.js
@@ -6,35 +6,51 @@ const personalNewsDB = {
   count: numberOfNews,
   news: {},
   actors: {},
-  genres: [], 
+  genres: [],
   shop: {},
   privat: false,
 };
 
+//// For method
 
-for(let i = 0; i < 1; i++) {
+// for(let i = 0; i < 1; i++) {
+//   const a = prompt("Oxirgi ko`rgan yangiliklarizdan bir?"),
+//         b = prompt("Unga qancha baho bergan bo`lar ediz?");
+
+//         if( a != null && b != null && a != "" && b != "" && a.length < 50) {
+//           personalNewsDB.news[a] = b;
+//           console.log("Tayyor");
+//         } else{
+//           console.log("Error !");
+//           i--;
+//         }
+// }
+
+// While method
+
+let x = 0;
+
+while (x < 1) {
   const a = prompt("Oxirgi ko`rgan yangiliklarizdan bir?"),
-        b = prompt("Unga qancha baho bergan bo`lar ediz?");
+    b = prompt("Unga qancha baho bergan bo`lar ediz?");
 
-        if( a != null && b != null && a != "" && b != "" && a.length < 50) {
-          personalNewsDB.news[a] = b;
-          console.log("Tayyor");
-        } else{
-          console.log("Error !");
-          i--;
-        }
+  if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+    console.log("Tayyor !");
+  } else {
+    console.log(`Error !`);
+    x--;
+  }
+  x++;
 }
 
-if( personalNewsDB.count < 10 ){
+if (personalNewsDB.count < 10) {
   console.log(`Xabarni kam oqibsiz`);
-} else if( personalNewsDB.count > 10 && personalNewsDB.count < 30 ) {
+} else if (personalNewsDB.count > 10 && personalNewsDB.count < 30) {
   console.log(`Siz klassik tomoshabinsiz!`);
-} else if( personalNewsDB.count > 30 ) {
+} else if (personalNewsDB.count > 30) {
   console.log(`Siz yangiliklar ishqibozisiz!`);
-} else{
+} else {
   console.log(`Xatolik yuz berdi!`);
 }
-
-
 
 console.log(personalNewsDB);
