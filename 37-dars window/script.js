cffc3045a57b7c05257a2851ba368a46297a5270
Mr-Dostonbek <goyipovdoston0@gmail.com>
2021-08-21 16:51:08 +0500

    37-lesson finish..

--- 37-dars window/script.js
+++ 37-dars window/script.js
@@ -1,7 +1,8 @@
 "use strict";
 
 const box = document.querySelector(".box"),
-      btn = document.querySelector("button");
+      btn = document.querySelector("button"),
+      nav = document.querySelector("nav");
 
 btn.addEventListener('click', () => {
   // box.style.height = box.scrollHeight + "px";
@@ -10,4 +11,12 @@ btn.addEventListener('click', () => {
     console.log("Hello");
   }
 
+});
+
+window.addEventListener('scroll', () => {
+  if(window.scrollY > 300){
+    nav.classList.add("fixed");
+  } else {
+    nav.classList.remove("fixed");
+  }
 });
