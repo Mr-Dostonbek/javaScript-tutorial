4a00d922860a2deee6f3db45380f5dd502213f93
Mr-Dostonbek <goyipovdoston0@gmail.com>
2021-09-08 18:30:34 +0300

    43-lessn start..

--- 43-dars/js/script.js
+++ 43-dars/js/script.js
@@ -1,11 +1,20 @@
 "use strict";
 
 window.addEventListener('DOMContentLoaded', () => {
+
+    // LOADER
     const loader = document.querySelector('.loader');
     setTimeout(function() {
         loader.style.opacity = "0";
         setTimeout(function() {
             loader.style.display = "none";
-        }, 2000);
-    }, 3500);
+        }, 1500);
+    }, 2000);
+
+    // TABS
+
+    const tabs = document.querySelectorAll('.tabheader__item'),
+        tabContent = document.querySelectorAll('.tabcontent'),
+        headerParents = document.querySelector('.tabheader__items');
+
 });
